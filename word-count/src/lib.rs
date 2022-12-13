6c01699892feb1ae900fac2afdb4ef2ddf0d9284
Bruce Chen <bruce30262@pm.me>
2022-12-14 00:10:21 +0800

    Add comments for Word Count

--- word-count/src/lib.rs
+++ word-count/src/lib.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
 
 pub fn word_count(words: &str) -> HashMap<String, u32> {
-    words.split(|c: char| !c.is_alphanumeric() && c != '\'')
-        .filter(|w| !w.is_empty())
-        .map(|w| w.trim_matches('\'').to_lowercase())
-        .fold(HashMap::new(), |mut acc, w| {
+    words.split(|c: char| !c.is_alphanumeric() && c != '\'') // split with chars that matches the condition
+        .filter(|w| !w.is_empty()) // strip empty string. filter for iter, retain for vec
+        .map(|w| w.trim_matches('\'').to_lowercase()) // map for iter. Do trim & to_lowercase
+        .fold(HashMap::new(), |mut acc, w| { // fold for iter. Fold with HashMap::new() ( acc )
             acc.entry(w).and_modify(|cnt| *cnt += 1).or_insert(1);
             acc
         })
