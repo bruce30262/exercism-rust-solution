4375e915622b2a2ef04cab446e98bd5e3e46cf22
Bruce Chen <bruce30262@pm.me>
2022-12-24 16:41:25 +0800

    Improve Tournament with clippy

--- tournament/src/lib.rs
+++ tournament/src/lib.rs
@@ -32,23 +32,28 @@ impl Record {
 
 // watch out the lifetime of the variables
 fn update_score<'a>(table: &mut HashMap<&'a str, Record>, win_team: &'a str, lose_team: &'a str, score:u32) {
+    // update win_team record
     let (win_score, win_record) = if score == 3 { (3, 'W') } else { (1, 'D') };
-    table.entry(win_team).or_insert(Record::new());
-    table.get_mut(win_team).map(|val| val.update_wdl(&win_record)); // use get_mut so value is mutable
-    table.get_mut(win_team).map(|val| val.update_score(win_score));
-    
+    table.entry(win_team).or_default(); // Insert a default Record struct if win_team not exist
+    if let Some(record) = table.get_mut(win_team) { // use get_mut so value is mutable
+        record.update_wdl(&win_record);
+        record.update_score(win_score);
+    }
+    // update lose_team record
     let (lose_score, lose_record) = if score == 3 { (0, 'L') } else { (1, 'D') };
-    table.entry(lose_team).or_insert(Record::new());
-    table.get_mut(lose_team).map(|val| val.update_wdl(&lose_record));
-    table.get_mut(lose_team).map(|val| val.update_score(lose_score));
+    table.entry(lose_team).or_default();
+    if let Some(record) = table.get_mut(lose_team) {
+        record.update_wdl(&lose_record);
+        record.update_score(lose_score);
+    }
 }
 
 pub fn tally(match_results: &str) -> String {
     let mut table: HashMap<&str, Record> = HashMap::new();
-    let matches = match_results.split("\n");
+    let matches = match_results.split('\n');
     
     for m in matches {
-        if m.len() > 0 {
+        if !m.is_empty() {
             let mut iter = m.split(';');
             let t1 = iter.next().unwrap();
             let t2 = iter.next().unwrap();
@@ -63,7 +68,7 @@ pub fn tally(match_results: &str) -> String {
     }
 
     let mut table_vec: Vec<(&&str, &Record)> = table.iter().collect();
-    table_vec.sort_by(|a, b| b.1.p.cmp(&a.1.p).then(a.0.cmp(&b.0))); // first record.p, then team name
+    table_vec.sort_by(|a, b| b.1.p.cmp(&a.1.p).then(a.0.cmp(b.0))); // first record.p, then team name
 
     let mut result = "Team                           | MP |  W |  D |  L |  P".to_owned();
     for (team, record) in table_vec {
